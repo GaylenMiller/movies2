cfb2a3452f91e6e5d053630372b120876f90e19a
aundra miller and Dustin Davis <hiitsdustindavis@gmail.com>
2016-01-26 15:44:53 -0800

    added specs, scripts, and html

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,60 @@ function Ticket(movie, age, time) {
   this.movie = movie;
   this.age = age;
   this.time = time;
-}
+
+  var ticket = [movie, age, time]
+};
+
+prototype function { age.price + time.price } 
+
+function Age(youth, student, adult, senior) {
+  this.youth = youth;
+  this.student = student;
+  this.adult = adult;
+  this.senior = senior;
+};
+
+// Movies Object
+var movie1 = { name: "Homeward Bound"};
+var movie2 = { name: "The Martian"};
+var movie3 = { name: "Groundhog Day"};
+
+var movies = { selection: [movie1, movie2, movie3] };
+
+// Age Group Object
+var youth = { age: "youth", price: 3.50 };
+var student = { age: "student", price: 6.50 };
+var adult = { age: "adult", price: 100.00 };
+var senior = { age: "senior", price: 6.50 };
+
+var age = { groups: [youth, student, adult, senior] };
+
+//Time Selections
+var matinee = {time: "2:00PM" price: -1.00};
+var afternoon = {time: "4:30PM"};
+var evening = {time: "8:00PM"};
+
+var time = { times: [matinee, afternoon, evening] };
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+$("form#ticket-purchase").submit(function(event){
+  event.preventDefault();
+
+  var movies = $('#movie').val();
+  var age = $('#age').val();
+  var time = $('#time').val();
+  var tickets = new Ticket(movies, age, time);
+
+  tickets.forEach(function(ticket) {
+    tickets.age
+  });
+
+});
+});
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,15 @@
 describe('Movies', function() {
   it('captures user data', function(){
-    var testTicket = new Ticket("Homeward Bound", "45", "7:00pm");
+    var testTicket = new Ticket("Homeward Bound", "Adult", "7:00pm");
     expect(testTicket.movie).to.equal("Homeward Bound");
-    expect(testTicket.age).to.equal("45");
+    expect(testTicket.age).to.equal("Adult");
     expect(testTicket.time).to.equal("7:00pm");
+  });
+  it('captures input data for age', function(){
+    var testAge = new Age("youth", "student", "adult", "senior");
+    expect(testAge.youth).to.equal("youth");
+    expect(testAge.student).to.equal("student");
+    expect(testAge.adult).to.equal("adult");
+    expect(testAge.senior).to.equal("senior");
   });
 });
